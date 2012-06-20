63b0a6bb1e7455ca04fd6d6c05c01366acd22f1f
Chris Charabaruk <chris@charabaruk.com>
2012-06-20 16:45:07 -0400

    basic RelayCommand and ViewModelBase implementations

---
+++ MvvmSample/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace MvvmSample
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+    }
+}
